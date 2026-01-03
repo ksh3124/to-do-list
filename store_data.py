fc173cc86ffdb324f43dfb5dff7c90af37be4beb
K S HARSHAVARDHAN <ksharshavardhan3124@gmail.com>
2026-01-03 19:15:06 +0530

    Update load and save tasks with data directory
    Refactor task loading and saving to use a dynamic file path based on the user's app data directory.

--- store_data.py
+++ store_data.py
@@ -1,17 +1,25 @@
-import os
 import json
+import os
+import sys
+
+APP_NAME = "To Do by ksh"
+
+def get_data_dir():
+    appdata = os.getenv("APPDATA")
+    data_dir = os.path.join(appdata, APP_NAME)
+    os.makedirs(data_dir, exist_ok=True)
+    return data_dir
+
+FILE_PATH = os.path.join(get_data_dir(), "tasks.json")
+
 
 def load_tasks():
-    if not os.path.exists("tasks.json"):
+    if not os.path.exists(FILE_PATH):
         return {}
+    with open(FILE_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
 
-    try:
-        with open("tasks.json", "r") as file:
-            return json.load(file)
-    except json.JSONDecodeError:
-        return {}
 
 def save_tasks(tasks):
-    with open("tasks.json", "w") as file:
-        json.dump(tasks, file, indent=4)
-
+    with open(FILE_PATH, "w", encoding="utf-8") as f:
+        json.dump(tasks, f, indent=4)
